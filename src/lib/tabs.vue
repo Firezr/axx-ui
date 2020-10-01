a24e547e5d5786c38c358541d26ec26e31b37ea4
曾锐 <zengr666@163.com>
2020-10-01 22:47:49 +0800

    feat: add component

---
+++ src/lib/tabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="gulu-switch"  :class="{'gulu-checked': false}">
+    <span></span>
+  </button>
+</template>
+<script lang="ts" setup="props, context">
+import { ref, SetupContext } from "vue";
+export default {
+  props: {
+    value: Boolean,
+  },
+};
+
+</script>
+
+<style lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+.gulu-switch {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
+  }
+  &.gulu-checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
+  }
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
+  }
+  &.gulu-checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
+  }
+}
+</style>
