399ead993d755cd6c7994f3c18d2661bb70f2bba
曾锐 <zengr666@163.com>
2020-10-06 17:54:25 +0800

    refactor: extract markdown.vue

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,20 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import Doc from "./views/Doc.vue";
-import Intro from "./views/Intro.vue";
-import Install from "./views/Install.vue";
-import GetStarted from "./views/GetStarted.vue";
+// import Intro from "./views/Intro.vue";
+// import Install from "./views/Install.vue";
+// import GetStarted from "./views/GetStarted.vue";
+
 
 import DocDefault from "./components/DocDefault.vue";
 import SwitchDemo from "./components/switch/SwitchDemo.vue";
 import ButtonDemo from "./components/button/ButtonDemo.vue";
 import DialogDemo from "./components/dialog/DialogDemo.vue";
 import TabsDemo from "./components/tabs/TabsDemo.vue";
+
+import { h } from "vue";
+import Markdown from "./components/Markdown.vue";
+const md = (path) => h(Markdown, { path: `../markdown/${path}.md`, key: path })
 
 let history = createWebHashHistory()
 export default createRouter({
@@ -30,15 +35,19 @@ export default createRouter({
         },
         {
           path: 'intro',
-          component: Intro
+          // component: Intro,
+          component: md('intro'),
         },
         {
           path: 'install',
-          component: Install
+          // component: Install,
+          component: md('install'),
+          
         },
         {
           path: 'get-started',
-          component: GetStarted
+          // component: GetStarted,
+          component: md('get-started'),
         },
         // 组件列表
         {
